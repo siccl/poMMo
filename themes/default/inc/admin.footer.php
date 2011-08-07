d8e93f504aade11c816b0448ee100a9a0b14ccb4
jamesarbrown <jamesarbrown@43-78.ahf.data.net.uk>
2011-08-07 09:57:34 +0100

    Modified admin.header and footer to contain code that was previously dynamically pushed into there by smarty in the javascript ui

--- themes/default/inc/admin.footer.php
+++ themes/default/inc/admin.footer.php
@@ -1,9 +1,27 @@
-		<br class="clear" />
-	</div> <!-- end content -->
-	<div id="footer">
-		<p>
-			- <?php echo _('Page fueled by poMMo mailing management software'); ?> -
-		</p>
-	</div> <!-- end footer -->
+<br class="clear" />
+</div> <!-- end content -->
+<div id="footer">
+    <p>
+        - <?php echo _('Page fueled by poMMo mailing management software'); ?> -
+    </p>
+</div> <!-- end footer -->
+
+<?php
+//Called by javascripts.
+if ($dialogImageCache)
+{
+?>
+    <div class="imgCache">
+        <img src="{$url.theme.shared}images/loader.gif" />
+        <img src="{$url.theme.shared}images/dialog/close.gif" />
+        <img src="{$url.theme.shared}images/dialog/close_hover.gif" />
+        <img src="{$url.theme.shared}images/dialog/sprite.gif" />
+        <img src="{$url.theme.shared}images/dialog/bl.gif" />
+        <img src="{$url.theme.shared}images/dialog/br.gif" />
+        <img src="{$url.theme.shared}images/dialog/bc.gif" />
+    </div>
+<?php 
+}; 
+?>
 </body>
 </html>
--- themes/default/inc/admin.header.php
+++ themes/default/inc/admin.header.php
@@ -1,51 +1,73 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" /> 
-<title><?php echo $this->title; ?></title>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" /> 
+        <title><?php echo $this->title; ?></title>
 
-<script src="<?php echo $this->url['theme']['shared'] ?>js/jq/jquery.js"
-      type="text/javascript"></script>
-<script src="<?php echo $this->url['theme']['shared'] ?>js/pommo.js"
-      type="text/javascript"></script>
-<script type="text/javascript">
-    poMMo.confirmMsg = '<?php echo _('Are you sure?'); ?>';
-</script>
+        <script src="<?php echo $this->url['theme']['shared'] ?>js/jq/jquery.js"
+        type="text/javascript"></script>
+        <script src="<?php echo $this->url['theme']['shared'] ?>js/pommo.js"
+        type="text/javascript"></script>
+        <script type="text/javascript">
+            poMMo.confirmMsg = '<?php echo _('Are you sure?'); ?>';
+        </script>
+        <?php
+        //These javascripts are only included if we set them = true in the calling form
+        if ($isUiForm)
+        {
+            include 'ui.form.php';
+        }
+        if ($isUiDialog)
+        {
+            include 'ui.dialog.php';
+        }
+        if ($isUiTabs)
+        {
+            include 'ui.tabs.php';
+        }
+        if ($isUiSlider)
+        {
+            include 'ui.slider.php';
+        }
+        if ($isUiGrid)
+        {
+            include 'ui.grid.php';
+        }
+        ?>
 
-<link type="text/css" rel="stylesheet" 
-      href="<?php echo $this->url['theme']['shared']?>css/default.admin.css" />
+        <link type="text/css" rel="stylesheet" 
+              href="<?php echo $this->url['theme']['shared'] ?>css/default.admin.css" />
 
-<head>	
-</head>
+        <head>	
+        </head>
 
-<body>
+        <body>
 
-<div id="header">
+            <div id="header">
 
-<h1><a href="<?php echo($this->config['site_url']) ?>">
-       <img src="<?php echo($this->url['theme']['shared']); ?>images/pommo.gif" 
-         alt="pommo logo" /> <strong><?php echo($this->config['site_name']) ?></strong>
-    </a></h1>
-</div>
+                <h1><a href="<?php echo($this->config['site_url']) ?>">
+                        <img src="<?php echo($this->url['theme']['shared']); ?>images/pommo.gif" 
+                             alt="pommo logo" /> <strong><?php echo($this->config['site_name']) ?></strong>
+                    </a></h1>
+            </div>
 
-<ul id="menu">
-<li><a href="<?php echo($this->url_base) ?>index.php?logout=TRUE">
-        <?php echo _('Logout')?></a></li>
-<li class="advanced"><a href="<?php echo($this->url_base) ?>support.php">
-        <?php echo _('Support')?></a></li>
-<li><a href="<?php echo($this->url_base) ?>admin.php">
-        <?php echo _('Admin Page')?></a></li>
-</ul>
+            <ul id="menu">
+                <li><a href="<?php echo($this->url_base) ?>index.php?logout=TRUE">
+                        <?php echo _('Logout') ?></a></li>
+                <li class="advanced"><a href="<?php echo($this->url_base) ?>support.php">
+                        <?php echo _('Support') ?></a></li>
+                <li><a href="<?php echo($this->url_base) ?>admin.php">
+            <?php echo _('Admin Page') ?></a></li>
+            </ul>
 
-<?php 
-    if ($this->sidebar !== false)
-    {
-        include($this->template_dir.'/inc/admin.sidebar.php');
-        echo('<div id="content">');
-    } else 
-    {
-        echo('<div id="content" class="wide">');
-    }
-?>
+            <?php
+            if ($this->sidebar !== false)
+            {
+                include($this->template_dir.'/inc/admin.sidebar.php');
+                echo('<div id="content">');
+            } else
+            {
+                echo('<div id="content" class="wide">');
+            }
+            ?>
 
-        
