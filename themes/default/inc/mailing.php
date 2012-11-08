138cc81ae431a3e670e8fef7968519a22883ec03
Adrian Ancona Novelo <soonick5@yahoo.com.mx>
2012-11-08 11:46:22 -0600

    Pass selected charset to mailing preview page. Fixes #94

--- themes/default/inc/mailing.php
+++ themes/default/inc/mailing.php
@@ -1,8 +1,13 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 <head>
+<?php
+    if (empty($this->charset)) {
+        $this->charset = $this->list_charset;
+    }
+?>
 <meta content="text/html;charset=<?php echo $this->charset; ?>"
-		http-equiv="Content-Type">
+        http-equiv="Content-Type">
 <title><?php echo $this->subject; ?></title>
 </head>
 <body>
