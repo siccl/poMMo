3d4101158e5a64aaf22e139d2768bdcfee2a85f3
Adrian Ancona <soonick5@yahoo.com.mx>
2013-10-07 20:24:35 -0700

    Fixing some phpcs errors

--- ajax/ajax.addtemplate.php
+++ ajax/ajax.addtemplate.php
@@ -1,8 +1,9 @@
 <?php
 /**
- *  Original Code Copyright (C) 2005, 2006, 2007, 2008  Brice Burgess <bhb@iceburg.net>
+ *  Original Code Copyright (C) 2005, 2006, 2007, 2008  Brice Burgess
+ *  <bhb@iceburg.net>
  *  released originally under GPLV2
- * 
+ *
  *  This file is part of poMMo.
  *
  *  poMMo is free software: you can redistribute it and/or modify
@@ -17,7 +18,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with Pommo.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  *  This fork is from https://github.com/soonick/poMMo
  *  Please see docs/contribs for Contributors
  *
@@ -25,7 +26,7 @@
 
 
 /**********************************
-	INITIALIZATION METHODS
+  INITIALIZATION METHODS
  *********************************/
 require '../bootstrap.php';
 require_once Pommo::$_baseDir.'classes/Pommo_Mailing_Template.php';
@@ -35,52 +36,52 @@ $logger = Pommo::$_logger;
 $dbo = Pommo::$_dbo;
 
 /**********************************
-	SETUP TEMPLATE, PAGE
+  SETUP TEMPLATE, PAGE
  *********************************/
 require_once Pommo::$_baseDir.'classes/Pommo_Template.php';
 $view = new Pommo_Template();
 
-if (empty ($_POST))
-{
-	// ___ USER HAS NOT SENT FORM ___
+if (empty ($_POST)) {
+    // ___ USER HAS NOT SENT FORM ___
 
-	$vMsg = array ();
-	$vMsg['name'] = Pommo::_T('Cannot be empty.');
-	$view->assign('vMsg', $vMsg);
-	
-}
-else
-{
-	// ___ USER HAS SENT FORM ___
-	require_once Pommo::$_baseDir.'classes/Pommo_Validate.php';
-	$validator = new Pommo_Validate();
-    $validator->setPost($_POST);
-    $validator->addData('name', 'Other', false);
+    $vMsg = array ();
+    $vMsg['name'] = Pommo::_T('Cannot be empty.');
+    $view->assign('vMsg', $vMsg);
+} else {
+    // ___ USER HAS SENT FORM ___
+    include_once Pommo::$_baseDir.'classes/Pommo_Validate.php';
+    $validator = new Pommo_Validate();
+      $validator->setPost($_POST);
+      $validator->addData('name', 'Other', false);
 
-	if ($result = $validator->checkData())
-	{
-		// __ FORM IS VALID
+    if ($result = $validator->checkData()) {
+        // __ FORM IS VALID
 
-		$t = Pommo_Mailing_Template::make(array(
-			'name' => $_POST['name'],
-			'description' => $_POST['description'],
-			'body' => Pommo::$_session['state']['mailing']['body'],
-			'altbody' => Pommo::$_session['state']['mailing']['altbody']
-		));
-		$id = Pommo_Mailing_Template::add($t);
-		
-		if ($id) {
-			$logger->addMsg(sprintf(Pommo::_T('Template %s saved.'),'<strong>'.$_POST['name'].'</strong>'));
-			$view->assign('success',true);
-		}
-		else
-			$logger->addMsg(Pommo::_T('Error with addition.'));
-		
-		
-	} else {
-		// __ FORM NOT VALID
-		$logger->addMsg(Pommo::_T('Please choose a name for your template.'));
-	}
+        $t = Pommo_Mailing_Template::make(
+            array(
+                'name' => $_POST['name'],
+                'description' => $_POST['description'],
+                'body' => Pommo::$_session['state']['mailing']['body'],
+                'altbody' => Pommo::$_session['state']['mailing']['altbody']
+            )
+        );
+        $id = Pommo_Mailing_Template::add($t);
+
+        if ($id) {
+            $logger->addMsg(
+                sprintf(
+                    Pommo::_T('Template %s saved.'),
+                    '<strong>' . $_POST['name'] . '</strong>'
+                )
+            );
+            $view->assign('success', true);
+        } else {
+            $logger->addMsg(Pommo::_T('Error with addition.'));
+        }
+    } else {
+        // __ FORM NOT VALID
+        $logger->addMsg(Pommo::_T('Please choose a name for your template.'));
+    }
 }
 
 $view->display('admin/mailings/mailing/ajax.addtemplate');
--- ajax/ajax.mailingtest.php
+++ ajax/ajax.mailingtest.php
@@ -2,7 +2,7 @@
 /**
  *  Original Code Copyright (C) 2005, 2006, 2007, 2008  Brice Burgess <bhb@iceburg.net>
  *  released originally under GPLV2
- * 
+ *
  *  This file is part of poMMo.
  *
  *  poMMo is free software: you can redistribute it and/or modify
@@ -17,79 +17,72 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with Pommo.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  *  This fork is from https://github.com/soonick/poMMo
  *  Please see docs/contribs for Contributors
  *
  */
- 
+
  /**********************************
-	INITIALIZATION METHODS
+  INITIALIZATION METHODS
  *********************************/
 require '../bootstrap.php';
 require_once Pommo::$_baseDir.'classes/Pommo_Fields.php';
 require_once Pommo::$_baseDir.'classes/Pommo_Mailing.php';
 
-Pommo::init(array('keep' => TRUE));
+Pommo::init(array('keep' => true));
 $logger = Pommo::$_logger;
 $dbo = Pommo::$_dbo;
 
 /**********************************
-	SETUP TEMPLATE, PAGE
+  SETUP TEMPLATE, PAGE
  *********************************/
 require_once Pommo::$_baseDir.'classes/Pommo_Template.php';
 $view = new Pommo_Template();
 
 $current = Pommo_Mailing::isCurrent();
 
-if (empty ($_POST))
-{
-	// ___ USER HAS NOT SENT FORM ___
-	$vMsg = array ();
-	$vMsg['email'] = Pommo::_T('Invalid email address');
-	$view->assign('vMsg', $vMsg);
-}
-else
-{
-	// ___ USER HAS SENT FORM ___
-	require_once Pommo::$_baseDir.'classes/Pommo_Validate.php';
-	$validator = new Pommo_Validate();
-    $validator->setPost($_POST);
-    $validator->addData('email', 'Email', false);
+if (empty ($_POST)) {
+    // ___ USER HAS NOT SENT FORM ___
+    $vMsg = array ();
+    $vMsg['email'] = Pommo::_T('Invalid email address');
+    $view->assign('vMsg', $vMsg);
+} else {
+    // ___ USER HAS SENT FORM ___
+    include_once Pommo::$_baseDir . 'classes/Pommo_Validate.php';
+    $validator = new Pommo_Validate();
+      $validator->setPost($_POST);
+      $validator->addData('email', 'Email', false);
 
-	if ($result = $validator->checkData() && !$current)
-	{
-		// __ FORM IS VALID
-		require_once Pommo::$_baseDir.'classes/Pommo_Mail_Ctl.php';
-		require_once Pommo::$_baseDir.'classes/Pommo_Subscribers.php';
-		require_once Pommo::$_baseDir.'classes/Pommo_Validate.php';
+    if ($result = $validator->checkData() && !$current) {
+        // __ FORM IS VALID
+        include_once Pommo::$_baseDir . 'classes/Pommo_Mail_Ctl.php';
+        include_once Pommo::$_baseDir . 'classes/Pommo_Subscribers.php';
+        include_once Pommo::$_baseDir . 'classes/Pommo_Validate.php';
 
-		// get a copy of the message state
-		// composition is valid (via preview.php)
-		$state = Pommo::$_session['state']['mailing'];
+        // get a copy of the message state
+        // composition is valid (via preview.php)
+        $state = Pommo::$_session['state']['mailing'];
 
-		// create temp subscriber
-		$subscriber = array(
-			'email' => $_POST['email'],
-			'registered' => time(),
-			'ip' => $_SERVER['REMOTE_ADDR'],
-			'status' => 0,
-			'data' => $_POST['d']
-		);
-		Pommo_Validate::subscriberData(
-			$subscriber['data'],
-			array('active' => FALSE, 'ignore' => TRUE, 'log' => false)
-		);
-		$key = Pommo_Subscribers::add($subscriber);
-		if (!$key)
-		{
-			$logger->addErr('Unable to Add Subscriber');
-		}
-		else
-		{
-			// temp subscriber created
-			$state['tally'] = 1;
-			$state['group'] = _('Test Mailing');
+        // create temp subscriber
+        $subscriber = array(
+          'email' => $_POST['email'],
+          'registered' => time(),
+          'ip' => $_SERVER['REMOTE_ADDR'],
+          'status' => 0,
+          'data' => $_POST['d']
+        );
+        Pommo_Validate::subscriberData(
+            $subscriber['data'],
+            array('active' => false, 'ignore' => true, 'log' => false)
+        );
+        $key = Pommo_Subscribers::add($subscriber);
+        if (!$key) {
+            $logger->addErr('Unable to Add Subscriber');
+        } else {
+            // temp subscriber created
+            $state['tally'] = 1;
+            $state['group'] = _('Test Mailing');
 
             if ('off' == $state['ishtml']) {
                 $state['body'] = $state['altbody'];
@@ -98,55 +91,53 @@ else
                 $state['ishtml'] = 'on';
             }
 
-			// create mailing
-			$mailing = Pommo_Mailing::make(array(), TRUE);
-			$state['status'] = 1;
-			$state['current_status'] = 'stopped';
-			$state['command'] = 'restart';
-			$state['charset'] = $state['list_charset'];
-			$mailing = Pommo_Helper::arrayIntersect($state, $mailing);
-			$code = Pommo_Mailing::add($mailing);
+            // create mailing
+            $mailing = Pommo_Mailing::make(array(), true);
+            $state['status'] = 1;
+            $state['current_status'] = 'stopped';
+            $state['command'] = 'restart';
+            $state['charset'] = $state['list_charset'];
+            $mailing = Pommo_Helper::arrayIntersect($state, $mailing);
+            $code = Pommo_Mailing::add($mailing);
 
-			// populate queue
-			$queue = array($key);
-			if (!Pommo_Mail_Ctl::queueMake($queue))
-			{
-				$logger->addErr('Unable to Populate Queue');
-			}
-
-			// spawn mailer
-			else if (!Pommo_Mail_Ctl::spawn(Pommo::$_baseUrl.
-					'ajax/mailings_send4.php?test=TRUE&code='.$code))
-			{
-				$logger->addErr('Unable to spawn background mailer');
-			}
-			else
-			{
-				$view->assign('sent',$_POST['email']);
-			}
-		}
-	}
-	elseif ($current)
-	{
-		$logger->addMsg(_('A mailing is currently taking place. Please try again later.'));
-		$view->assign($_POST);
-	}
-	else
-	{
-		// __ FORM NOT VALID
-		$logger->addMsg(_('Please review and correct errors with your submission.'));
-		$view->assign($_POST);
-	}
+            // populate queue
+            $queue = array($key);
+            if (!Pommo_Mail_Ctl::queueMake($queue)) {
+                $logger->addErr('Unable to Populate Queue');
+            } else if (!Pommo_Mail_Ctl::spawn(
+                Pommo::$_baseUrl .
+                'ajax/mailings_send4.php?test=TRUE&code='.$code
+            )) {
+                $logger->addErr('Unable to spawn background mailer');
+            } else {
+                $view->assign('sent', $_POST['email']);
+            }
+        }
+    } elseif ($current) {
+        $logger->addMsg(
+            _('A mailing is currently taking place. Please try again later.')
+        );
+        $view->assign($_POST);
+    } else {
+        // __ FORM NOT VALID
+        $logger->addMsg(_('Please review and correct errors with your submission.'));
+        $view->assign($_POST);
+    }
 }
 
-if (Pommo::$_config['demo_mode'] == 'on')
-{
-	$logger->addMsg(sprintf(_('%sDemonstration Mode%s is on -- no Emails will actually'
-			.' be sent. This is good for testing settings.'),
-			'<a href="'.Pommo::$_baseUrl.'setup_configure.php#mailings">',
-			'</a>'));
+if (Pommo::$_config['demo_mode'] == 'on') {
+    $logger->addMsg(
+        sprintf(
+            _(
+                '%sDemonstration Mode%s is on -- no Emails will actually'
+                . ' be sent. This is good for testing settings.'
+            ),
+            '<a href="'.Pommo::$_baseUrl.'setup_configure.php#mailings">',
+            '</a>'
+        )
+    );
 }
 
-$view->assign('fields',Pommo_Fields::get());
+$view->assign('fields', Pommo_Fields::get());
 $view->display('admin/mailings/mailing/ajax.mailingtest');
 
